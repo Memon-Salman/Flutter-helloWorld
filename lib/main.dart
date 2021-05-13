9d374131514f25ec0c627d71d3057f7fa081df8d
Memon-Salman <76585035+Memon-Salman@users.noreply.github.com>
2021-05-13 23:40:45 +0500

    flutter helloWorld Assignment-1

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+
+// Main Function
+void main() {
+  runApp(MyApp());
+}
+// Class
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Text("Salman Memon"),
+      
+      ),
+    );
+  }
+}
+
+// Salman Memon
